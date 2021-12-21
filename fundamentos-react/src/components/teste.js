9c14d65326e8998862780bcb123d89009676fa43
oliveiradiney <dineyoliveira1999@gmail.com>
2021-12-20 22:04:02 -0300

    Desafio megasena #2

--- fundamentos-react/src/components/teste.js
+++ fundamentos-react/src/components/teste.js
@@ -4,25 +4,6 @@ function sum(a,b){
 
 console.log(sum(3,4))
 
-function gerarNumeroNaoContido(min, max, array){
-    const aleatorio = parseInt(Math.random() * (max + 1 - min)) + min 
-    return array.includes(aleatorio) ? 
-        gerarNumeroNaoContido(min, max, array) :
-        aleatorio
-}
 
-function gerarNumeros(qtd){
-    const numeros = Array(qtd)
-        .fill(0)
-        .reduce((nums) => {
-            const novoNum = gerarNumeroNaoContido(1, 60, nums)
-            return [ ...nums, novoNum]
-        }, [])
-        .sort((n1 ,n2) => n1-n2)
-
-
-
-    return numeros
-}
 
 console.log(gerarNumeros(7))
